60487eb6f640b73faac74737c64bace3fdcd27b6
miyamoto <miyasato131msta@gmail.com>
2020-09-16 09:34:34 +0900

    commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,32 +2,22 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
+
 CarrierWave.configure do |config|
-
-  if Rails.env.production?
-
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'inkgrow'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:AKIAJMWSAPVAFKV5VAMQ],
-    aws_secret_access_key: Rails.application.credentials.aws[:secreh6wud1IDgKRl7SHzuheeCJBLtW0bXSQgMW4Qwmst_access_key],
-    region: 'ap-northeast-1'
-
-      path_style: true
-  }
-
-  config.fog_directory = 'inkgrow'
-
-config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/inkgrow'
-
-else
-
-config.storage :file
-
-config.enable_processing = false if Rails.env.test?
-
-end
-
+  if Rails.env.production? || Rails.env.development? # 開発中もs3使う
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'inkgrow'
+    config.asset_host = 'https://s3.amazonaws.com/inkgrow'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAJMWSAPVAFKV5VAMQ',
+      aws_secret_access_key: 'secreh6wud1IDgKRl7SHzuheeCJBLtW0bXSQgMW4Qwmst_access_key',
+      region: 'ap-northeast-1',
+      # path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,8 +272,13 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :twitter, Rails.application.credentials.twitter[:twitter_api_key], Rails.application.credentials.twitter[:twitter_api_secret], display: 'popup'
-  # ==> Warden configuration
+  
+  
+  # config.omniauth :twitter, Rails.application.credentials.twitter[:NnAhEteuMgaggepuoZrwP9zri],
+  #                           Rails.application.credentials.twitter[:h6h75d4HryTbxN8SxO3swgao6i2kG43I8LBNB7rQKCC798S4gy]
+  
+  
+                            # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
